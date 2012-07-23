82a46631f5312ffead5ae47b19c2233f13bbae47
Adam Crabtree <adam.crabtree@redrobotlabs.com>
2012-07-23 14:25:07 -0700

    Implment no-op forwarding proxy

--- proxis.js
+++ proxis.js
@@ -28,32 +28,75 @@ Q.wrap = function wrap(promise, callback) {
 };
 
 
-function $xy(reified, opts, that) {
-    if (typeof reified !== 'object' && typeof reified !== 'function') return reified;
+function $xy(obj, opts, that) {
+    // Don't proxy immutables
+    if (['undefined', 'boolean', 'number', 'string'].indexOf(typeof obj) !== -1) return obj;
 
 	opts = opts || {};
 
     var handler = {
+        // Custom fundamental traps
+        // Forward compatibility when getPropertyDescriptor becomes available?
         getPropertyDescriptor: function(name) {
-            return Object.getOwnPropertyDescriptor(reified, name);
+            var desc = (Object.getPropertyDescriptor || Object.getOwnPropertyDescriptor).call(Object, obj, name);
+            if (typeof desc !== 'undefined') { desc.configurable = true}
+            return desc;
         },
+        // Custom derived traps
+        // Keep proxying for all properties
+        get: function(receiver, name) {
+            return $xy(obj[name], opts, obj);
+        },
+
+        // From http://wiki.ecmascript.org/doku.php?id=harmony:proxies&s=proxy#examplea_no-op_forwarding_proxy
+        // Fundamental traps
         getOwnPropertyDescriptor: function(name) {
-            var desc = Object.getOwnPropertyDescriptor(reified, name);
-            if (desc.configurable !== true) return;
+            var desc = Object.getOwnPropertyDescriptor(obj, name);
+            if (typeof desc !== 'undefined') { desc.configurable = true}
             return desc;
         },
         getOwnPropertyNames: function() {
-            return Object.getOwnPropertyNames(reified);
+            return Object.getOwnPropertyNames(obj);
         },
         getPropertyNames: function() {
-            return Object.getOwnPropertyNames(reified);
+            return Object.getOwnPropertyNames(obj);
         },
-        get: function(receiver, name) {
-            return $xy(reified[name], opts, reified);
+        // Derived traps
+        defineProperty: function(name, desc) {
+            Object.defineProperty(obj, name, desc);
+        },
+        delete: function(name) {
+            return delete obj[name];
+        },
+        fix: function() {
+            if (Object.isFrozen(obj)) {
+                var result = {};
+                Object.getOwnPropertyNames(obj).forEach(function(name) {
+                    result[name] = Object.getOwnPropertyDescriptor(obj, name);
+                });
+                return result;
+            }
+            // As long as obj is not frozen, the proxy won't allow itself to be fixed
+            return undefined; // will cause a TypeError to be thrown
+        },
+        has: function(name) { return name in obj; },
+        hasOwn: function(name) { return ({}).hasOwnProperty.call(obj, name); },
+        set: function(receiver, name, val) { obj[name] = val; return true; }, // bad behavior when set fails in non-strict mode
+        enumerate: function() {
+            var result = [];
+            for (var name in obj) {
+                result.push(name);
+            }
+            return result;
+        },
+        keys: function() {
+            return Object.keys(obj);
         }
     };
+
+    // The actual function proxying
     var fn = function() {
-        var args = [reified, that].concat(Array.prototype.slice.call(arguments));
+        var args = [obj, that].concat(Array.prototype.slice.call(arguments));
         if (opts.isLazy === true) {
             return function() {
                 return Q.ncall.apply(Q, args);
@@ -63,11 +106,11 @@ function $xy(reified, opts, that) {
         }
     }
 
-	return typeof reified === 'function' ? Proxy.createFunction(handler, fn, fn ) : Proxy.create(handler);
+	return typeof obj === 'function' ? Proxy.createFunction(handler, fn, fn ) : Proxy.create(handler);
 }
 
-$xy.lazy = function(reified) {
-	return $xy(reified, {isLazy: true});
+$xy.lazy = function(obj) {
+	return $xy(obj, {isLazy: true});
 };
 
 $xy.__proto__ = Q;
